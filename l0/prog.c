ed580350e3b926aea067910df0f9b343253ace04
Shevja <vlsdki@gmail.com>
2020-10-02 14:42:15 +0700

    init l0 l1

---
+++ l0/prog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <mpi.h>
+int main(int argc, char **argv){
+    int rank, commsize;MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("Hello, I am process %d of %d\n", rank, commsize);
+    MPI_Finalize();
+    
+    return 0;
+}
